dcba2b3c6acb4da33485901bcbb2c9baf0c7d56b
aphrem-thomas <aphremthomas@gmail.com>
2019-03-12 00:47:01 +0530

    rechart added

--- sensor-monitoring/src/App.js
+++ sensor-monitoring/src/App.js
@@ -3,6 +3,13 @@ import Graph from './components/Graph';
 import './App.css';
 
 class App extends Component {
+  componentDidMount(){
+    this.connection = new WebSocket('wss://echo.websocket.org');
+    this.connection.onopen = o => console.log(o);
+    this.connection.onmessage = evt => { 
+      console.log(evt);
+    }
+  }
   render() {
     return (
       <div className="App">
--- sensor-monitoring/src/components/Graph.js
+++ sensor-monitoring/src/components/Graph.js
@@ -1,15 +1,64 @@
 import React, { Component } from 'react';
-import {VictoryAnimation,VictoryBar, VictoryAxis, VictoryChart} from 'victory';
+import {AreaChart,XAxis,YAxis,Area,Tooltip} from 'recharts';
 
 
+const rangeData = [
+  {
+    "day": "05-01",
+    "temperature": -1
+  },
+  {
+    "day": "05-02",
+    "temperature": 2
+  },
+  {
+    "day": "05-03",
+    "temperature": 3
+  },
+  {
+    "day": "05-04",
+    "temperature": 4
+  },
+  {
+    "day": "05-05",
+    "temperature": 12
+  },
+  {
+    "day": "05-06",
+    "temperature": 5
+  },
+  {
+    "day": "05-07",
+    "temperature": 3
+  },
+  {
+    "day": "05-08",
+    "temperature": 0
+    
+  },
+  {
+    "day": "05-09",
+    "temperature": -3
+  }
+]
+  
 class Graph extends Component {
   render() {
     return (
       <div className="Graph">
-       <VictoryChart>
-           <VictoryAxis/>
-           <VictoryBar/>
-       </VictoryChart>
+        <AreaChart
+          width={730}
+          height={250}
+          data={rangeData}
+          margin={{
+            top: 20, right: 20, bottom: 20, left: 20,
+          }}
+        >
+          <XAxis dataKey="day" />
+          <YAxis />
+          <Area dataKey="temperature" stroke="#8884d8" fill="#8884d8" />
+          <Tooltip />
+        </AreaChart>
       </div>
     );
   }
