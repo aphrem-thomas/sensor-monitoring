5e82815ad95fa56006e8c02fc71429bd7dd979c9
aphrem-thomas <aphremthomas@gmail.com>
2019-03-13 01:10:54 +0530

    commited

--- sensor-monitoring/src/App.js
+++ sensor-monitoring/src/App.js
@@ -3,17 +3,24 @@ import Graph from './components/Graph';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={data:null}
+  }
   componentDidMount(){
-    this.connection = new WebSocket('wss://echo.websocket.org');
+    this.connection = new WebSocket("ws://192.168.43.181:5678/");
     this.connection.onopen = o => console.log(o);
     this.connection.onmessage = evt => { 
-      console.log(evt);
+      this.setState({data:evt.data})
+      console.log("recef");
     }
+    this.connection.onclose=()=>console.log("closed");
   }
   render() {
     return (
       <div className="App">
        <Graph/>
+       <h2>{this.state.data}</h2>
       </div>
     );
   }
