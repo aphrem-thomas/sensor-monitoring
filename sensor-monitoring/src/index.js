ee078b625411f0048fbd5cb140e25580cfd68cff
aphremthomas <aphremthomas@gmail.com>
2019-03-26 20:44:01 +0530

    redux added

--- sensor-monitoring/src/index.js
+++ sensor-monitoring/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore,combineReducers,} from 'redux';
 
 ReactDOM.render(
     <BrowserRouter>
